7dab56acde16c328418669c91f4fcb92eea91bbd
MaraGheorghe <mara.gheorghe@stud.acs.upb.ro>
2026-05-08 18:24:57 +0300

    Add files via upload

---
+++ src/lab_m1/Tema1/breakout.cpp
@@ -0,0 +1,313 @@
+#include <random>
+#include "lab_m1/Tema1/breakout.h"
+#include <iostream>
+
+using namespace breakout;
+using namespace std;
+
+Breakout::Breakout()
+{
+}
+
+Breakout::~Breakout()
+{
+}
+
+void Breakout::Init(float X, float Y, float width, float height, float unit,
+    std::unordered_map<std::string, Mesh*>& meshes)
+{
+    this->X = X;
+    this->Y = Y;
+    this->width = width;
+    this->height = height;
+    this->unit = unit;
+
+    shipX = 0.5 * (width - shipWidth);
+    shipY = Y + 0.5 * unit;
+
+    ballRadius = 0.2 * unit;
+    ballX = 0.5 * width;
+    ballY = shipY + shipHeight + ballRadius;
+    ballAngle = RADIANS(45);
+
+    meshes["ball"] = shapes::circle(glm::vec3(X, Y, 1), ballRadius, 360, "ball", glm::vec3(0.2, 0.7, 0.5), true);
+
+    brickWidth = width / brickColumns;
+    brickHeight = (height - unit) / 2 / brickRows;
+    brickGridX = X;
+    brickGridY = Y + height - brickRows * brickHeight - unit;
+
+    // red border around the top text and hearts
+    upperX = X;
+    upperY = Y + height - unit;
+    meshes["upper_border"] = shapes::rectangle(glm::vec3(0, 0, 0),
+        width, unit, "upper_border", glm::vec3(1, 0, 0), false);
+
+    // brickX = brick with a durability of X
+    meshes["brick1"] = shapes::rectangle(glm::vec3(0, 0, 0), brickWidth, brickHeight,
+        "brick1", glm::vec3(1, 1, 0), true);
+    meshes["brick2"] = shapes::rectangle(glm::vec3(0, 0, 0), brickWidth, brickHeight,
+        "brick2", glm::vec3(1, 0.5, 0), true);
+    meshes["brick3"] = shapes::rectangle(glm::vec3(0, 0, 0), brickWidth, brickHeight,
+        "brick3", glm::vec3(0, 1, 1), true);
+    meshes["brick4"] = shapes::rectangle(glm::vec3(0, 0, 0), brickWidth, brickHeight,
+        "brick4", glm::vec3(0, 0.5, 1), true);
+    meshes["brick5"] = shapes::rectangle(glm::vec3(0, 0, 0), brickWidth, brickHeight,
+        "brick5", glm::vec3(1, 0, 1), true);
+
+    /*
+    * bricks are assigned a durability level based on their location:
+    * first row: 1
+    * first third of rows: 1 or 2
+    * middle third of rows: 2 or 3
+    * last third of rows: 3 or 4
+    * last row: 4 or 5
+    */
+    random_device rd;
+    mt19937 gen(rd());
+    for (int i = 0; i < brickRows; i++)
+    {
+        for (int j = 0; j < brickColumns; j++)
+        {
+            if (i == 0)
+                brickGrid[i][j] = 1;
+            else if (i < brickRows / 3)
+            {
+                uniform_int_distribution<> distrib(1, 2);
+                brickGrid[i][j] = distrib(gen);
+            }
+            else if (i < 2 * brickRows / 3)
+            {
+                uniform_int_distribution<> distrib(2, 3);
+                brickGrid[i][j] = distrib(gen);
+            }
+            else if (i < brickRows - 1)
+            {
+                uniform_int_distribution<> distrib(3, 4);
+                brickGrid[i][j] = distrib(gen);
+            }
+            else
+            {
+                uniform_int_distribution<> distrib(4, 5);
+                brickGrid[i][j] = distrib(gen);
+            }
+        }
+    }
+
+    meshes["filledHeart"] = shapes::heart(glm::vec3(0, 0, 0), 0.75 * unit, 0.75 * unit,
+        "filledHeart", glm::vec3(0.8, 0, 0), true);
+
+    meshes["emptyHeart"] = shapes::heart(glm::vec3(0, 0, 0), 0.75 * unit, 0.75 * unit,
+        "filledHeart", glm::vec3(0.8, 0, 0), false);
+
+    livesLeft = 3;
+    score = 0;
+}
+
+void Breakout::gameLogicUpdate(float deltaTimeSceonds)
+{
+    if (!gameRunning)
+        return;
+
+    wallHit();
+
+    shipHit();
+
+    brickHit();
+
+    // updates brick shrinking logic
+    for (int i = 0; i < brickRows; i++)
+        for (int j = 0; j < brickColumns; j++)
+        {
+            if (brickGrid[i][j] <= 0)
+            {
+                if (brickGrid[i][j] > -1)
+                    brickGrid[i][j] -= brickShrinkSpeed * deltaTimeSceonds;
+                else
+                    brickGrid[i][j] = -1;
+            }
+        }
+
+    ballX += cos(ballAngle) * ballSpeed;
+    ballY += sin(ballAngle) * ballSpeed;
+}
+
+void Breakout::wallHit()
+{
+    // check if the ball hit the bottom wall
+    if (ballY - ballRadius <= Y)
+    {
+        livesLeft--;
+        gameRunning = false;
+
+        if (livesLeft > 0)
+            resetPlayer();
+    }
+
+    // check if the ball hit the upper wall
+    else if (ballY + ballRadius >= brickGridY + brickHeight * brickRows)
+    {
+        changeAngleHorizontal();
+        ballY = brickGridY + brickHeight * brickRows - ballRadius;  // prevent sticking
+
+    }
+    // check if the ball hit the left wall
+    else if (ballX <= X)
+    {
+        changeAngleVertical();
+        ballX = X + ballRadius;  // prevent sticking
+    }
+
+    // check if the ball hit the right wall
+    else if (ballX + ballRadius > X + width)
+    {
+        changeAngleVertical();
+        ballX = X + width - ballRadius;  // prevent sticking
+    }
+}
+
+void Breakout::shipHit()
+{
+    // check if the ball hit the ship's top
+    if (ballY - ballRadius < shipY + shipHeight &&
+        ballY > shipY &&
+        ballX >= shipX &&
+        ballX <= shipX + shipWidth)
+    {
+        ballAngle = (ballX - shipX) / (shipWidth / 2);
+        ballY = shipY + shipHeight + ballRadius;  // prevent sticking
+        return;
+    }
+
+    // check if the ball hit the ship's left side
+    if (ballY <= shipY + shipHeight &&
+            ballY >= shipY &&
+            ballX <= shipX &&
+            ballX + ballRadius > shipX)
+    {
+        changeAngleVertical();
+        changeAngleHorizontal();
+        ballX = shipX - 1.5 * ballRadius;  // prevent sticking
+        return;
+    }
+
+    // check if the ball hit the ship's right side
+    if (ballY <= shipY + shipHeight &&
+            ballY >= shipY &&
+            ballX >= shipX + shipWidth &&
+            ballX - ballRadius < shipX + shipWidth)
+    {
+        changeAngleVertical();
+        changeAngleHorizontal();
+        ballX = shipX + shipWidth + 1.5 * ballRadius;  // prevent sticking
+        return;
+    }
+}
+
+void Breakout::brickHit()
+{
+    // ball contact points
+    float left = ballX - ballRadius;
+    float right = ballX + ballRadius;
+    float bottom = ballY - ballRadius;
+    float top = ballY + ballRadius;
+
+    int i, j;
+
+    // check for bottom collision
+    i = (bottom - brickGridY) / brickHeight;
+    j = (ballX - brickGridX) / brickWidth;
+    if (i >= 0 && i < brickRows && j >= 0 && j < brickColumns && brickGrid[i][j] > 0)
+    {
+        brickGrid[i][j]--;
+        changeAngleHorizontal();
+
+        ballY = (brickGridY + (i + 1) * brickHeight) + ballRadius;  // prevent sticking
+        raiseScore();
+        return;
+    }
+
+    // check for left collision
+    i = (ballY - brickGridY) / brickHeight;
+    j = (left - brickGridX) / brickWidth;
+    if (i >= 0 && i < brickRows && j >= 0 && j < brickColumns && brickGrid[i][j] > 0)
+    {
+        brickGrid[i][j]--;
+        changeAngleVertical();
+
+        ballX = (brickGridX + (j + 1) * brickWidth) + ballRadius;  // prevent sticking
+        raiseScore();
+        return;
+    }
+
+    // check for right collision
+    i = (ballY - brickGridY) / brickHeight;
+    j = (right - brickGridX) / brickWidth;
+    if (i >= 0 && i < brickRows && j >= 0 && j < brickColumns && brickGrid[i][j] > 0)
+    {
+        brickGrid[i][j]--;
+        changeAngleVertical();
+
+        ballX = (brickGridX + j * brickWidth) - ballRadius;  // prevent sticking
+        raiseScore();
+        return;
+    }
+
+    // check for top collision
+    i = (top - brickGridY) / brickHeight;
+    j = (ballX - brickGridX) / brickWidth;
+    if (i >= 0 && i < brickRows && j >= 0 && j < brickColumns && brickGrid[i][j] > 0)
+    {
+        brickGrid[i][j]--;
+        changeAngleHorizontal();
+
+        ballY = (brickGridY + i * brickHeight) - ballRadius;  // prevent sticking
+        raiseScore();
+        return;
+    }
+
+}
+
+void Breakout::moveShip(const std::string& direction)
+{
+    if (direction == "right" && shipX + shipWidth < X + width)
+        shipX += shipSpeed;
+    if (direction == "left" && shipX > X)
+        shipX -= shipSpeed;
+}
+
+void Breakout::changeAngleHorizontal()
+{
+    ballAngle = RADIANS(360) - ballAngle;
+    if (ballAngle >= RADIANS(360))
+        ballAngle -= RADIANS(360);
+    if (ballAngle < 0)
+        ballAngle += RADIANS(360);
+}
+
+void Breakout::changeAngleVertical()
+{
+    ballAngle = RADIANS(180) - ballAngle;
+    if (ballAngle >= RADIANS(360))
+        ballAngle -= RADIANS(360);
+    if (ballAngle < 0)
+        ballAngle += RADIANS(360);
+}
+
+void Breakout::resetPlayer()
+{
+    shipX = 0.5 * (width - shipWidth);
+    shipY = Y + 0.5 * unit;
+
+    ballX = 0.5 * width;
+    ballY = shipY + shipHeight + ballRadius;
+    ballAngle = RADIANS(45);
+}
+
+void Breakout::raiseScore()
+{
+    bricksLeft--;
+    score++;
+    if (score > highScore)
+        highScore = score;
+}
---
+++ src/lab_m1/Tema1/shapes.cpp
@@ -0,0 +1,145 @@
+#include "lab_m1/Tema1/shapes.h"
+
+Mesh* shapes::rectangle(glm::vec3 bottomLeftCorner, float length, float height,
+    const std::string& name, glm::vec3 color, bool fill)
+{
+    std::vector<VertexFormat> vertices =
+    {
+        VertexFormat(bottomLeftCorner, color),
+        VertexFormat(bottomLeftCorner + glm::vec3(length, 0, 0), color),
+        VertexFormat(bottomLeftCorner + glm::vec3(length, height, 0), color),
+        VertexFormat(bottomLeftCorner + glm::vec3(0, height, 0), color)
+    };
+
+    Mesh* rectangle = new Mesh(name);
+    std::vector<unsigned int> indices = { 0, 1, 2, 3 };
+
+    if (!fill) {
+        rectangle->SetDrawMode(GL_LINE_LOOP);
+    }
+    else {
+        indices.push_back(0);
+        indices.push_back(2);
+    }
+
+    rectangle->InitFromData(vertices, indices);
+    return rectangle;
+}
+
+Mesh* shapes::start_button(glm::vec3 bottomLeftCorner, float length, float height,
+    const std::string& name, bool fill, bool validBuild)
+{
+    glm::vec3 color;
+    if (validBuild)
+        color = glm::vec3(0, 1, 0);
+    else
+        color = glm::vec3(1, 0, 0);
+
+    std::vector<VertexFormat> vertices =
+    {
+        VertexFormat(bottomLeftCorner, color),
+        VertexFormat(bottomLeftCorner + glm::vec3(length, 0, 0), color),
+        VertexFormat(bottomLeftCorner + glm::vec3(length / 2, height / 2, 0), color),
+        VertexFormat(bottomLeftCorner + glm::vec3(length, height, 0), color),
+        VertexFormat(bottomLeftCorner + glm::vec3(0, height, 0), color)
+    };
+
+    Mesh* button = new Mesh(name);
+    std::vector<unsigned int> indices = { 0, 1, 2, 3, 4 };
+
+    if (!fill) {
+        button->SetDrawMode(GL_LINE_LOOP);
+    }
+    else {
+        indices.push_back(0);
+    }
+
+    button->InitFromData(vertices, indices);
+    return button;
+}
+
+Mesh* shapes::circle(glm::vec3 center, float radius, float segments, 
+    const std::string& name, glm::vec3 color, bool fill)
+{
+    Mesh* circle = new Mesh(name);
+    std::vector<VertexFormat> vertices{};
+    std::vector<unsigned int> indices{};
+
+    if (fill)
+    {
+        vertices.push_back(VertexFormat(center, color));
+        indices.push_back(0);
+    }
+
+    for (int i = 0; i < segments; i++)
+    {
+        float angle = i * (RADIANS(360) / segments);
+        vertices.push_back(VertexFormat(
+            center + glm::vec3(radius * sin(angle), radius * cos(angle), 0), color));
+
+        if (fill)
+            indices.push_back(i + 1);
+        else
+            indices.push_back(i);
+    }
+
+    if (!fill) {
+        circle->SetDrawMode(GL_LINE_LOOP);
+    }
+    else
+    {
+        circle->SetDrawMode(GL_TRIANGLE_FAN);
+        indices.push_back(1);
+    }
+    circle->InitFromData(vertices, indices);
+    return circle;
+}
+
+Mesh* shapes::heart(glm::vec3 bottomLeftCorner, float length, float height,
+    const std::string& name, glm::vec3 color, bool fill)
+{
+    Mesh* heart = new Mesh(name);
+    std::vector<VertexFormat> vertices{};
+    std::vector<unsigned int> indices{};
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(0.5f * length, 0, 0), color));
+    indices.push_back(0);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(length, 2.0f / 5.0f * height, 0), color));
+    indices.push_back(1);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(length, 4.0f / 5.0f * height, 0), color));
+    indices.push_back(2);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(5.0f / 6.0f * length, height, 0), color));
+    indices.push_back(3);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(4.0f / 6.0f * length, height, 0), color));
+    indices.push_back(4);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(0.5f * length, 4.0f / 5.0f * height, 0), color));
+    indices.push_back(5);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(2.0f / 6.0f * length, height, 0), color));
+    indices.push_back(6);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(1.0f / 6.0f * length, height, 0), color));
+    indices.push_back(7);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(0, 4.0f / 5.0f * height, 0), color));
+    indices.push_back(8);
+
+    vertices.push_back(VertexFormat(bottomLeftCorner + glm::vec3(0, 2.0f / 5.0f * height, 0), color));
+    indices.push_back(9);
+
+    if (!fill) {
+        heart->SetDrawMode(GL_LINE_LOOP);
+    }
+    else
+    {
+        heart->SetDrawMode(GL_TRIANGLE_FAN);
+        indices.push_back(1);
+    }
+    heart->InitFromData(vertices, indices);
+    return heart;
+}
